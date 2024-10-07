a8e15b602d42dc76c8f7d3798dc31afaee76d1e1
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2024-10-07 13:46:12 -0400

    Corrections for "def raw_data(df)"

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -178,21 +178,29 @@ def raw_data(df):   # shows raw data
     print('\nCalculating User Stats...\n')
     start_time = time.time()
 
-    row = -5    # initial row
-    column = 0  # initial column
-    #show_raw_data = ""  # TEMPORARY
+    row = 0    # initial row
+    column = 5  # initial column
+    show_raw_data = "y"  # TEMPORARY
     show_raw_data = input("Do you want to see 5 lines of raw data (y or n): ").lower()
     print(show_raw_data)    # DELETE LATER'''
     while show_raw_data != "y" and show_raw_data != "n":    # Same while loop
         show_raw_data = input("Please choose one of the following options (y or n): ").lower()
+    print("Past invalid while loop")    # DELETE LATER'''
     while show_raw_data == "y":
-        print(df.iloc[row + 5, column + 5])
+        try:
+            print(df.iloc[row:column])
+        except :
+            print("You have reached the last 5 lines.")
+            break
         show_raw_data = input("Do you want to see another 5 lines of raw data (y or n): ").lower()
         if show_raw_data == "n":
             break
         elif show_raw_data != "y" and show_raw_data != "n":
             while show_raw_data != "y" and show_raw_data != "n":    # Same while loop
                 show_raw_data = input("Please choose one of the following options (y or n): ").lower()
+        else:   # if show_raw_data == "y"
+            row += 5
+            column += 5
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-' * 40)
